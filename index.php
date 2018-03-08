17b3aa261d2a1847a85099deadfd50cbdd8f8b58
Sudarshan Maharjan <sud.mrjn@gmail.com>
2018-03-08 21:14:43 +0100

    Small Change Test
    This is Small Change

--- index.php
+++ index.php
@@ -5,12 +5,10 @@
 <title>Facebook like Tag Friends</title>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
- 
     
  <script type="text/javascript">
 $(document).ready(function()
 {
-
 var start=/@/ig;
 var word=/@(\w+)/ig;
 
